95bd18ba938ea6543612247a4227acfa0627259b
Ed Dela Cruz <edmundo.delacruz@cartrack.com>
2025-04-16 21:58:06 +0800

    Commit as of 04/16/2025

--- src/main/java/pageEvents/loginPageEvents.java
+++ src/main/java/pageEvents/loginPageEvents.java
@@ -1,5 +1,7 @@
 package pageEvents;
 
+import java.util.Dictionary;
+
 import base.BaseTest;
 import pageObjects.loginPageElements;
 
@@ -7,6 +9,23 @@ public class loginPageEvents extends BaseTest {
 	
 	public void registerButton() {
 		click(loginPageElements.tabRegister);
+	}
+	
+	public void login(@SuppressWarnings("rawtypes") Dictionary loginDetails) {
+		//Username and Password
+		clear(loginPageElements.txtUsername);
+		sendKeys(loginPageElements.txtUsername, loginDetails.get("userName").toString());
+		clear(loginPageElements.txtPassword);
+		sendKeys(loginPageElements.txtPassword, loginDetails.get("password").toString());
+		
+		//Click Submit
+		click(loginPageElements.btnSubmit);
+		
+	}
+	
+	public void validateSuccessfullyLogin() {
+		assertElementIsDisplayed(loginPageElements.hdrLoginSuccessfully);
+		
 	}
 
 }
